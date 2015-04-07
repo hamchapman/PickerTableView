31cac2f7e0d252a2ef74dee72363fe1490ebf1c2
Hamilton Chapman <hamchapman@gmail.com>
2015-04-07 10:48:33 +0100

    Got customizable number of rows working, nearly

--- TableViewTesting/ViewController.swift
+++ TableViewTesting/ViewController.swift
@@ -33,15 +33,24 @@ class ViewController: UIViewController {
         SecondsTableView.dataSource = c2
         SecondsTableView.delegate = c2
         
+        MinutesTableView.separatorStyle = UITableViewCellSeparatorStyle.None
+        
+        MinutesTableView.backgroundColor = UIColor.redColor()
+        
         MinutesTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         SecondsTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
-    }
+        
+        let numberOfRows: Float = 5.0
+        let rowHeight = Float(MinutesTableView.frame.height) / numberOfRows
 
+        MinutesTableView.rowHeight = CGFloat(rowHeight)
+        SecondsTableView.rowHeight = CGFloat(rowHeight)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 }
-
 
 class IntervalPickerController: NSObject, UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate  {
     let tableData: Array<Dictionary<String, String>>!
@@ -50,6 +59,7 @@ class IntervalPickerController: NSObject, UITableViewDataSource, UITableViewDele
     init(tableData: Array<Dictionary<String, String>>, inout tableView: UITableView!) {
         self.tableData = tableData
         self.tableView = tableView
+//        self.tableView.decelerationRate = UIScrollViewDecelerationRateFast
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -60,51 +70,58 @@ class IntervalPickerController: NSObject, UITableViewDataSource, UITableViewDele
         return tableData.count
     }
     
-    func scrollViewWillBeginDecelerating(scrollView: UIScrollView) {
-        println("Decelerating beginning")
-    }
-    
     func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
-        println("Ending decelrating")
-//        setTextOfMiddleElement(scrollView)
         scrollToCellTop(scrollView)
     }
     
     func scrollViewDidEndScrollingAnimation(scrollView: UIScrollView) {
-        println("Well hello there")
         setTextOfMiddleElement(scrollView)
     }
     
     func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        println("did end dragging")
         if !decelerate {
             scrollToCellTop(scrollView)
         }
     }
     
     func scrollToCellTop(scrollView: UIScrollView) {
-        var cellHeight = Int(tableView.rectForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)).height)
-        if (Int(scrollView.contentOffset.y) % cellHeight) == 0 {
-            setTextOfMiddleElement(scrollView)
-        } else if ((Int(scrollView.contentOffset.y) % cellHeight) < (cellHeight / 2)) {
-            scrollView.setContentOffset(CGPointMake(0, CGFloat(Int(scrollView.contentOffset.y) - (Int(scrollView.contentOffset.y) % cellHeight))), animated: true)
-        } else {
-            scrollView.setContentOffset(CGPointMake(0, CGFloat(Int(scrollView.contentOffset.y) + cellHeight - (Int(scrollView.contentOffset.y) % cellHeight))), animated: true)
-        }
+        let tableViewOffset = scrollView.contentOffset.y
+        let visibleTableViewHeight = scrollView.frame.height
+        let cellHeight = tableView.rectForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)).height
+        
+        // TODO: Make it adjust either upwards or downwards depending on positioning, as per old code below... OR DOES IT ALREADY?
+
+//        var cellHeight = tableView.rectForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)).height
+//        if (tableViewOffset % cellHeight) == 0.0 {
+//            setTextOfMiddleElement(scrollView)
+//        } else if ((tableViewOffset % cellHeight) < (cellHeight / 2)) {
+//            scrollView.setContentOffset(CGPointMake(0, CGFloat(tableViewOffset - (tableViewOffset % cellHeight))), animated: true)
+//        } else {
+//            scrollView.setContentOffset(CGPointMake(0, CGFloat(tableViewOffset + cellHeight - (tableViewOffset % cellHeight))), animated: true)
+//        }
+        
+        let heightOfFullHiddenCellsAbove = tableViewOffset - (tableViewOffset % cellHeight)
+        let halfViewMinusMiddleCell = (visibleTableViewHeight - cellHeight) / 2
+        let offsetAdjustment = cellHeight - (halfViewMinusMiddleCell % cellHeight)
+        
+        scrollView.setContentOffset(CGPointMake(0, heightOfFullHiddenCellsAbove + offsetAdjustment), animated: true)
     }
     
     func setTextOfMiddleElement(scrollView: UIScrollView) {
-        var tableViewOffset = Int(scrollView.contentOffset.y) // x
-        var totalTableViewHeight = scrollView.contentSize.height // 2684
-        var visibleTableViewHeight = Int(scrollView.frame.height) //541
-        var cellHeight = Int(tableView.rectForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)).height) // cellHeight = 44
-        var cellsOnScreen = Int(visibleTableViewHeight / cellHeight)
+        // TODO: Get rid of constant casting to Int (only need to in forRow: call)
+        let tableViewOffset = Int(scrollView.contentOffset.y)
+        let totalTableViewHeight = scrollView.contentSize.height
+        let visibleTableViewHeight = Int(scrollView.frame.height)
+        let cellHeight = Int(tableView.rectForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)).height)
+        let cellsOnScreen = Int(visibleTableViewHeight / cellHeight)
         
-        var middleRowIndex = (cellsOnScreen / 2) + (tableViewOffset / cellHeight)
-        tableView.cellForRowAtIndexPath(NSIndexPath(forRow: middleRowIndex, inSection: 0))?.textLabel?.text = "Middle man"
+        let middleRowIndex = (cellsOnScreen / 2) + (tableViewOffset / cellHeight)
+        tableView.cellForRowAtIndexPath(NSIndexPath(forRow: middleRowIndex, inSection: 0))?.textLabel?.text = "Middle"
+        tableView.cellForRowAtIndexPath(NSIndexPath(forRow: middleRowIndex, inSection: 0))?.backgroundColor = UIColor.blueColor()
     }
     
     func scrollViewDidScroll(scrollView: UIScrollView) {
+        // TODO: Make this only run once after scrolling / dragging has begun
         for e in tableData {
             tableView.cellForRowAtIndexPath(NSIndexPath(forRow: e["index"]!.toInt()!, inSection: 0))?.textLabel?.text = e["index"]!
         }
@@ -113,8 +130,22 @@ class IntervalPickerController: NSObject, UITableViewDataSource, UITableViewDele
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
         let item = tableData[indexPath.row]
-    
+        
+        cell.backgroundColor = UIColor.clearColor()
+        
         cell.textLabel?.text = String(stringInterpolationSegment: item["text"]!)
+        let myFont = UIFont(name: "Arial", size: 40.0);
+        cell.textLabel!.font  = myFont;
         return cell
     }
+    
+//    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+//        println(tableView.cellForRowAtIndexPath(indexPath)?.frame.size.height)
+//    }
+    
+//    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+//        println(tableView.cellForRowAtIndexPath(indexPath)!.frame.size.height)
+//        return CGFloat(50.0)
+//    }
+    
 }
