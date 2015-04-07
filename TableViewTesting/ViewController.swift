e8d66e53c7406d2dfa84ae1cb8a55769e2eac6e6
Hamilton Chapman <hamchapman@gmail.com>
2015-04-08 00:43:09 +0100

    First stages of screwing around with generics

--- TableViewTesting/ViewController.swift
+++ TableViewTesting/ViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var c1: IntervalPickerController!
-    var c2: IntervalPickerController!
+    var c1: IntervalPickerController<Dictionary<String, String>>!
+    var c2: IntervalPickerController<Dictionary<String, String>>!
     
     @IBOutlet weak var MinutesTableView: UITableView!
     @IBOutlet weak var SecondsTableView: UITableView!
@@ -19,14 +19,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var intervalDictionaryData: Array<Dictionary<String, String>> = []
+        var timeUnits: Array<Dictionary<String, String>> = []
         
         for i in [Int](0...60) {
-            intervalDictionaryData.append(["index": String(i), "text": String(i)])
+            timeUnits.append(["index": String(i), "text": String(i)])
         }
         
-        c1 = IntervalPickerController(tableData: intervalDictionaryData, tableView: &MinutesTableView)
-        c2 = IntervalPickerController(tableData: intervalDictionaryData, tableView: &SecondsTableView)
+        let numberOfRows: Int = 8
+        
+        c1 = IntervalPickerController(tableData: timeUnits, tableView: &MinutesTableView, numberOfRows: numberOfRows)
+        c2 = IntervalPickerController(tableData: timeUnits, tableView: &SecondsTableView, numberOfRows: numberOfRows)
         
         MinutesTableView.dataSource = c1
         MinutesTableView.delegate = c1
@@ -40,8 +42,7 @@ class ViewController: UIViewController {
         MinutesTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         SecondsTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
-        let numberOfRows: Float = 5.0
-        let rowHeight = Float(MinutesTableView.frame.height) / numberOfRows
+        let rowHeight = Float(MinutesTableView.frame.height) / Float(numberOfRows)
 
         MinutesTableView.rowHeight = CGFloat(rowHeight)
         SecondsTableView.rowHeight = CGFloat(rowHeight)
@@ -52,13 +53,26 @@ class ViewController: UIViewController {
     }
 }
 
-class IntervalPickerController: NSObject, UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate  {
-    let tableData: Array<Dictionary<String, String>>!
+class IntervalPickerController<T>: NSObject, UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate  {
+//    let tableData: Array<Dictionary<String, String>>!
+    let tableData: [T]!
     let tableView: UITableView!
+    let numberOfRows: Int!
     
-    init(tableData: Array<Dictionary<String, String>>, inout tableView: UITableView!) {
+    init(tableData: [T], inout tableView: UITableView!, numberOfRows: Int) {
+//    init(tableData: Array<Dictionary<String, String>>, inout tableView: UITableView!, numberOfRows: Int) {
+        
+//        var loopedData: Array<Dictionary<String, String>> = []
+        var loopedData: [T] = []
+
+        for i in 1...50 {
+            loopedData += tableData
+        }
+        
         self.tableData = tableData
         self.tableView = tableView
+        self.numberOfRows = numberOfRows
+    
 //        self.tableView.decelerationRate = UIScrollViewDecelerationRateFast
     }
     
@@ -89,9 +103,20 @@ class IntervalPickerController: NSObject, UITableViewDataSource, UITableViewDele
         let visibleTableViewHeight = scrollView.frame.height
         let cellHeight = tableView.rectForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)).height
         
-        // TODO: Make it adjust either upwards or downwards depending on positioning, as per old code below... OR DOES IT ALREADY?
-
-//        var cellHeight = tableView.rectForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)).height
+        let heightOfFullHiddenCellsAbove = tableViewOffset - (tableViewOffset % cellHeight)
+        let halfViewMinusMiddleCell = (visibleTableViewHeight - cellHeight) / 2
+        let offsetAdjustment = cellHeight - (halfViewMinusMiddleCell % cellHeight)
+        
+        if self.numberOfRows % 2 == 1 {
+            if ((tableViewOffset % cellHeight) < (cellHeight / 2)) {
+                scrollView.setContentOffset(CGPointMake(0, CGFloat(tableViewOffset - (tableViewOffset % cellHeight))), animated: true)
+            } else {
+                scrollView.setContentOffset(CGPointMake(0, CGFloat(tableViewOffset + cellHeight - (tableViewOffset % cellHeight))), animated: true)
+            }
+        } else {
+            scrollView.setContentOffset(CGPointMake(0, CGFloat(tableViewOffset + (cellHeight / 2) - (tableViewOffset % cellHeight))), animated: true)
+        }
+        
 //        if (tableViewOffset % cellHeight) == 0.0 {
 //            setTextOfMiddleElement(scrollView)
 //        } else if ((tableViewOffset % cellHeight) < (cellHeight / 2)) {
@@ -100,30 +125,32 @@ class IntervalPickerController: NSObject, UITableViewDataSource, UITableViewDele
 //            scrollView.setContentOffset(CGPointMake(0, CGFloat(tableViewOffset + cellHeight - (tableViewOffset % cellHeight))), animated: true)
 //        }
         
-        let heightOfFullHiddenCellsAbove = tableViewOffset - (tableViewOffset % cellHeight)
-        let halfViewMinusMiddleCell = (visibleTableViewHeight - cellHeight) / 2
-        let offsetAdjustment = cellHeight - (halfViewMinusMiddleCell % cellHeight)
-        
-        scrollView.setContentOffset(CGPointMake(0, heightOfFullHiddenCellsAbove + offsetAdjustment), animated: true)
+//        scrollView.setContentOffset(CGPointMake(0, heightOfFullHiddenCellsAbove + offsetAdjustment), animated: true)
     }
     
     func setTextOfMiddleElement(scrollView: UIScrollView) {
         // TODO: Get rid of constant casting to Int (only need to in forRow: call)
-        let tableViewOffset = Int(scrollView.contentOffset.y)
+        let tableViewOffset = scrollView.contentOffset.y
         let totalTableViewHeight = scrollView.contentSize.height
-        let visibleTableViewHeight = Int(scrollView.frame.height)
-        let cellHeight = Int(tableView.rectForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)).height)
-        let cellsOnScreen = Int(visibleTableViewHeight / cellHeight)
+        let visibleTableViewHeight = scrollView.frame.height
+        let cellHeight = tableView.rectForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)).height
+        let cellsOnScreen = visibleTableViewHeight / cellHeight
         
         let middleRowIndex = (cellsOnScreen / 2) + (tableViewOffset / cellHeight)
-        tableView.cellForRowAtIndexPath(NSIndexPath(forRow: middleRowIndex, inSection: 0))?.textLabel?.text = "Middle"
-        tableView.cellForRowAtIndexPath(NSIndexPath(forRow: middleRowIndex, inSection: 0))?.backgroundColor = UIColor.blueColor()
+        tableView.cellForRowAtIndexPath(NSIndexPath(forRow: Int(middleRowIndex), inSection: 0))?.textLabel?.text = "Middle"
+        tableView.cellForRowAtIndexPath(NSIndexPath(forRow: Int(middleRowIndex), inSection: 0))?.backgroundColor = UIColor.blueColor()
     }
     
     func scrollViewDidScroll(scrollView: UIScrollView) {
         // TODO: Make this only run once after scrolling / dragging has begun
         for e in tableData {
             tableView.cellForRowAtIndexPath(NSIndexPath(forRow: e["index"]!.toInt()!, inSection: 0))?.textLabel?.text = e["index"]!
+        }
+    }
+    
+    subscript(input: T) -> String {
+        get {
+            return "test"
         }
     }
     
